505ca03770e9264fc771248bca5f0fbf8061d47d
k0rog <avramneoko6@gmail.com>
2022-07-11 22:53:41 +0300

    customer creation creates bank account as well

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -10,3 +10,15 @@ class BaseRepository(ABC):
     @abstractmethod
     def create_customer(self, data: dict):
         raise NotImplementedError
+
+    @abstractmethod
+    def create_bank_account(self, currency: str, balance: float = 0.0):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_unique_iban(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def check_bank_account(self, iban: str) -> bool:
+        raise NotImplementedError
